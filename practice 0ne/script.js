96296678050aeb17ae4e3207116afecd4d4a348d
Dareen Alsulami <DareenAlsulami11@gmail.com>
2024-02-25 18:26:51 +0300

    deleted:    index.html 	new file:   practice 0ne/index.html 	new file:   practice 0ne/script.js 	new file:   practice 0ne/stylesheet.css

---
+++ practice 0ne/script.js
@@ -0,0 +1,10 @@
+function mainComp(){
+    return (
+        <h1>hello i am learning react</h1>
+        
+    )
+
+}
+
+
+ReactDom.render( <div> <mainComp /></div>, document.getElementById('root'))
