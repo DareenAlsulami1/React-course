108bc3567d19dc14cfa5b481694b463d0d077aa7
Dareen Alsulami <DareenAlsulami11@gmail.com>
2024-03-23 03:13:03 +0300

    modified:   practice 0ne/index.html modified:   practice 0ne/script.js 	modified:   practice 0ne/stylesheet.css

--- practice 0ne/script.js
+++ practice 0ne/script.js
@@ -1,10 +1,10 @@
-function mainComp(){
-    return (
-        <h1>hello i am learning react</h1>
+// function mainComp(){
+//     return (
+//         <h1>hello i am learning react</h1>
         
-    )
+//     )
 
-}
+// }
 
 
-ReactDom.render( <div> <mainComp /></div>, document.getElementById('root'))
+ReactDOM.render(<div> <h1>hello and welcome</h1></div>, document.getElementById("root"))
